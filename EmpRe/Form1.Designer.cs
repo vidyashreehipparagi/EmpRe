f9b596f87192eb5ef8182a3fedbd42b541d91aa3
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-08-01 16:54:31 +0530

    Add project files.

---
+++ EmpRe/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace EmpRe
+{
+    partial class lbForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbEnterName = new System.Windows.Forms.Label();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.lbMobileNumber = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lLastName = new System.Windows.Forms.Label();
+            this.lbAddress = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lbCity = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.lbState = new System.Windows.Forms.Label();
+            this.lbRole = new System.Windows.Forms.Label();
+            this.lbGender = new System.Windows.Forms.Label();
+            this.RFemale = new System.Windows.Forms.RadioButton();
+            this.rMale = new System.Windows.Forms.RadioButton();
+            this.lbWorkMode = new System.Windows.Forms.Label();
+            this.rOnline = new System.Windows.Forms.RadioButton();
+            this.rOffine = new System.Windows.Forms.RadioButton();
+            this.lbSubmit = new System.Windows.Forms.Button();
+            this.lbDOB = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lbReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbEnterName
+            // 
+            this.lbEnterName.AutoSize = true;
+            this.lbEnterName.Location = new System.Drawing.Point(41, 9);
+            this.lbEnterName.Name = "lbEnterName";
+            this.lbEnterName.Size = new System.Drawing.Size(133, 16);
+            this.lbEnterName.TabIndex = 0;
+            this.lbEnterName.Text = "Enter Your Full Name";
+            this.lbEnterName.Click += new System.EventHandler(this.lbEnterName_Click);
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Location = new System.Drawing.Point(48, 42);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(72, 16);
+            this.lbFirstName.TabIndex = 1;
+            this.lbFirstName.Text = "First Name";
+            this.lbFirstName.Click += new System.EventHandler(this.lbFirstName_Click);
+            // 
+            // lbMobileNumber
+            // 
+            this.lbMobileNumber.AutoSize = true;
+            this.lbMobileNumber.Location = new System.Drawing.Point(335, 109);
+            this.lbMobileNumber.Name = "lbMobileNumber";
+            this.lbMobileNumber.Size = new System.Drawing.Size(99, 16);
+            this.lbMobileNumber.TabIndex = 2;
+            this.lbMobileNumber.Text = "Mobile Number";
+            this.lbMobileNumber.Click += new System.EventHandler(this.lbMobileNumber_Click);
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(48, 109);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(41, 16);
+            this.lbEmail.TabIndex = 3;
+            this.lbEmail.Text = "Email";
+            this.lbEmail.Click += new System.EventHandler(this.lbEmail_Click);
+            // 
+            // lLastName
+            // 
+            this.lLastName.AutoSize = true;
+            this.lLastName.Location = new System.Drawing.Point(325, 42);
+            this.lLastName.Name = "lLastName";
+            this.lLastName.Size = new System.Drawing.Size(72, 16);
+            this.lLastName.TabIndex = 4;
+            this.lLastName.Text = "Last Name";
+            this.lLastName.Click += new System.EventHandler(this.lLastName_Click);
+            // 
+            // lbAddress
+            // 
+            this.lbAddress.AutoSize = true;
+            this.lbAddress.Location = new System.Drawing.Point(48, 194);
+            this.lbAddress.Name = "lbAddress";
+            this.lbAddress.Size = new System.Drawing.Size(58, 16);
+            this.lbAddress.TabIndex = 5;
+            this.lbAddress.Text = "Address";
+            this.lbAddress.Click += new System.EventHandler(this.lbAddress_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(328, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 128);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 35);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(328, 128);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(232, 35);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(44, 299);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(200, 22);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(338, 289);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(215, 22);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(44, 223);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(516, 22);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "Reset";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // lbCity
+            // 
+            this.lbCity.AutoSize = true;
+            this.lbCity.Location = new System.Drawing.Point(58, 280);
+            this.lbCity.Name = "lbCity";
+            this.lbCity.Size = new System.Drawing.Size(29, 16);
+            this.lbCity.TabIndex = 13;
+            this.lbCity.Text = "City";
+            this.lbCity.Click += new System.EventHandler(this.lbCity_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(44, 384);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(161, 22);
+            this.textBox8.TabIndex = 14;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // lbState
+            // 
+            this.lbState.AutoSize = true;
+            this.lbState.Location = new System.Drawing.Point(335, 270);
+            this.lbState.Name = "lbState";
+            this.lbState.Size = new System.Drawing.Size(38, 16);
+            this.lbState.TabIndex = 15;
+            this.lbState.Text = "State";
+            this.lbState.Click += new System.EventHandler(this.lbState_Click);
+            // 
+            // lbRole
+            // 
+            this.lbRole.AutoSize = true;
+            this.lbRole.Location = new System.Drawing.Point(48, 365);
+            this.lbRole.Name = "lbRole";
+            this.lbRole.Size = new System.Drawing.Size(36, 16);
+            this.lbRole.TabIndex = 16;
+            this.lbRole.Text = "Role";
+            this.lbRole.Click += new System.EventHandler(this.lbRole_Click);
+            // 
+            // lbGender
+            // 
+            this.lbGender.AutoSize = true;
+            this.lbGender.Location = new System.Drawing.Point(48, 428);
+            this.lbGender.Name = "lbGender";
+            this.lbGender.Size = new System.Drawing.Size(52, 16);
+            this.lbGender.TabIndex = 17;
+            this.lbGender.Text = "Gender";
+            this.lbGender.Click += new System.EventHandler(this.lbGender_Click);
+            // 
+            // RFemale
+            // 
+            this.RFemale.AutoSize = true;
+            this.RFemale.Location = new System.Drawing.Point(166, 428);
+            this.RFemale.Name = "RFemale";
+            this.RFemale.Size = new System.Drawing.Size(74, 20);
+            this.RFemale.TabIndex = 18;
+            this.RFemale.TabStop = true;
+            this.RFemale.Text = "Female";
+            this.RFemale.UseVisualStyleBackColor = true;
+            this.RFemale.CheckedChanged += new System.EventHandler(this.RFemale_CheckedChanged);
+            // 
+            // rMale
+            // 
+            this.rMale.AutoSize = true;
+            this.rMale.Location = new System.Drawing.Point(166, 454);
+            this.rMale.Name = "rMale";
+            this.rMale.Size = new System.Drawing.Size(58, 20);
+            this.rMale.TabIndex = 19;
+            this.rMale.TabStop = true;
+            this.rMale.Text = "Male";
+            this.rMale.UseVisualStyleBackColor = true;
+            this.rMale.CheckedChanged += new System.EventHandler(this.rMale_CheckedChanged);
+            // 
+            // lbWorkMode
+            // 
+            this.lbWorkMode.AutoSize = true;
+            this.lbWorkMode.Location = new System.Drawing.Point(369, 428);
+            this.lbWorkMode.Name = "lbWorkMode";
+            this.lbWorkMode.Size = new System.Drawing.Size(87, 16);
+            this.lbWorkMode.TabIndex = 20;
+            this.lbWorkMode.Text = "Mode of work";
+            this.lbWorkMode.Click += new System.EventHandler(this.lbWorkMode_Click);
+            // 
+            // rOnline
+            // 
+            this.rOnline.AutoSize = true;
+            this.rOnline.Location = new System.Drawing.Point(475, 424);
+            this.rOnline.Name = "rOnline";
+            this.rOnline.Size = new System.Drawing.Size(66, 20);
+            this.rOnline.TabIndex = 21;
+            this.rOnline.TabStop = true;
+            this.rOnline.Text = "Online";
+            this.rOnline.UseVisualStyleBackColor = true;
+            this.rOnline.CheckedChanged += new System.EventHandler(this.rOnline_CheckedChanged);
+            // 
+            // rOffine
+            // 
+            this.rOffine.AutoSize = true;
+            this.rOffine.Location = new System.Drawing.Point(475, 454);
+            this.rOffine.Name = "rOffine";
+            this.rOffine.Size = new System.Drawing.Size(65, 20);
+            this.rOffine.TabIndex = 22;
+            this.rOffine.TabStop = true;
+            this.rOffine.Text = "Offline";
+            this.rOffine.UseVisualStyleBackColor = true;
+            this.rOffine.CheckedChanged += new System.EventHandler(this.rOffine_CheckedChanged);
+            // 
+            // lbSubmit
+            // 
+            this.lbSubmit.Location = new System.Drawing.Point(276, 481);
+            this.lbSubmit.Name = "lbSubmit";
+            this.lbSubmit.Size = new System.Drawing.Size(93, 56);
+            this.lbSubmit.TabIndex = 23;
+            this.lbSubmit.Text = "Submit";
+            this.lbSubmit.UseVisualStyleBackColor = true;
+            this.lbSubmit.Click += new System.EventHandler(this.lbSubmit_Click);
+            // 
+            // lbDOB
+            // 
+            this.lbDOB.AutoSize = true;
+            this.lbDOB.Location = new System.Drawing.Point(349, 365);
+            this.lbDOB.Name = "lbDOB";
+            this.lbDOB.Size = new System.Drawing.Size(36, 16);
+            this.lbDOB.TabIndex = 24;
+            this.lbDOB.Text = "DOB";
+            this.lbDOB.Click += new System.EventHandler(this.lbDOB_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(343, 384);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(210, 22);
+            this.dateTimePicker1.TabIndex = 25;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // lbReset
+            // 
+            this.lbReset.Location = new System.Drawing.Point(386, 481);
+            this.lbReset.Name = "lbReset";
+            this.lbReset.Size = new System.Drawing.Size(111, 56);
+            this.lbReset.TabIndex = 26;
+            this.lbReset.Text = "Edit";
+            this.lbReset.UseVisualStyleBackColor = true;
+            this.lbReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 549);
+            this.Controls.Add(this.lbReset);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lbDOB);
+            this.Controls.Add(this.lbSubmit);
+            this.Controls.Add(this.rOffine);
+            this.Controls.Add(this.rOnline);
+            this.Controls.Add(this.lbWorkMode);
+            this.Controls.Add(this.rMale);
+            this.Controls.Add(this.RFemale);
+            this.Controls.Add(this.lbGender);
+            this.Controls.Add(this.lbRole);
+            this.Controls.Add(this.lbState);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.lbCity);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbAddress);
+            this.Controls.Add(this.lLastName);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbMobileNumber);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.lbEnterName);
+            this.Name = "lbForm";
+            this.Text = "Employee Registration form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbEnterName;
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.Label lbMobileNumber;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lLastName;
+        private System.Windows.Forms.Label lbAddress;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lbCity;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label lbState;
+        private System.Windows.Forms.Label lbRole;
+        private System.Windows.Forms.Label lbGender;
+        private System.Windows.Forms.RadioButton RFemale;
+        private System.Windows.Forms.RadioButton rMale;
+        private System.Windows.Forms.Label lbWorkMode;
+        private System.Windows.Forms.RadioButton rOnline;
+        private System.Windows.Forms.RadioButton rOffine;
+        private System.Windows.Forms.Button lbSubmit;
+        private System.Windows.Forms.Label lbDOB;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button lbReset;
+    }
+}
+
---
+++ EmpRe/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmpRe
+{
+    public partial class lbForm : Form
+    {
+        public lbForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lbEnterName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbFirstName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lLastName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbMobileNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbAddress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbCity_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbState_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbRole_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbGender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RFemale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rMale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbWorkMode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rOnline_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rOffine_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbDOB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbSubmit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Registration Sucess");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
